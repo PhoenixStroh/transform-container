7e12ea2aa00441159a912271052789d99cc811ad
Martin Lande <martin.lande@hotmail.com>
2026-01-19 11:41:29 +0100

    Center visual pivot by default
    After using this addon for a while, I found that every single instance in my use case needed to use centered visual offset pivot. Eventually, I edited my local copy to be at 0.5, 0.5 by default to save some work.
    
    I suggest that this should be the default for the addon itself. It's a lot more common than cases where the top-left should be the default.

--- addons/transform_container/transform_container.gd
+++ addons/transform_container/transform_container.gd
@@ -33,7 +33,7 @@ var visual_scale := Vector2(1,1) :
 
 ## A percentage multiplied against any valid children's size and added to their pivot offset. Can include negative values and values farther than 0/1.
 @export 
-var visual_pivot_offset_perc := Vector2(0,0) :
+var visual_pivot_offset_perc := Vector2(0.5,0.5) :
 	set(value):
 		visual_pivot_offset_perc = value
 		queue_sort()
